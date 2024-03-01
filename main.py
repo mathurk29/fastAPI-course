c4772bac24aae7b3f373dd278160eb4d2bc97e0a
Kshitij Mathur <mathurk29@gmail.com>
2024-03-02 00:52:07 +0530

    create file hierarchy

--- main.py
+++ main.py
@@ -1,51 +1,8 @@
-from typing import Optional
+from fastapi import FastAPI
 
-from fastapi import FastAPI, HTTPException, status
-from pydantic import BaseModel
+from database_es import wsl_elasticsearch
 
+from routes import router
 app = FastAPI()
 
-id = 1
-database = dict()
-
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-    rating: Optional[int] = None
-
-
-@app.get('/')
-def root():
-    return {'OK'}
-
-
-@app.get('/posts')
-def get_posts():
-    return database
-
-
-@app.get('/posts/{id}')
-def get_posts_id(id: int):
-    print('here')
-    if id not in database:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Key {id} not found in: {database}")
-    return database[id]
-
-
-@app.post('/posts', status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post):
-    global id
-    database[id] = post.dict()
-    id = id + 1
-    return database
-
-
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete(id: int):
-    if id not in database:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Key {id} not found in: {database}")
-    database.pop(id)
+app.include_router(router)
---
+++ routes.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI, HTTPException, status
+
+from database_es import wsl_elasticsearch
+from database_dict import database, idx
+from model import Post
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@ router.get('/')
+def root():
+    return {'OK'}
+
+
+@ router.get('/posts')
+def get_posts():
+    return database
+
+
+@ router.get('/posts/{id}')
+def get_posts_id(id: int):
+    print('here')
+    if id not in database:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Key {id} not found in: {database}")
+    return database[id]
+
+
+@ router.post('/posts', status_code=status.HTTP_201_CREATED)
+def create_posts(post: Post):
+    global idx
+    database[idx] = post.dict()
+    idx = idx + 1
+    return database
+
+
+@ router.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete(id: int):
+    if id not in database:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Key {id} not found in: {database}")
+    database.pop(id)
+
+
+@ router.put("/posts/{id}", status_code=status.HTTP_201_CREATED)
+def update_post(id: int, post: Post):
+    return 'TBD'
