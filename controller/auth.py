5c3d46804e569a092b8ec542481b98a30f5c6cd4
Kshitij Mathur <mathurk29@gmail.com>
2024-03-10 16:49:44 +0530

    OAuth2PasswordRequestForm

--- controller/auth.py
+++ controller/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 import utils
@@ -10,13 +11,18 @@ login_router = APIRouter(tags=["Authentication"])
 
 
 @login_router.get("/login")
-def login(user_credential: schemas.UserCreate, db: Session = Depends(get_db)):
+def login(
+    user_credential: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db),
+):
     user = (
-        db.query(models.User).filter(models.User.email == user_credential.email).first()
+        db.query(models.User)
+        .filter(models.User.email == user_credential.username)
+        .first()
     )
     if user is None:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+            status_code=status.HTTP_403_FORBIDDEN, detail="User not found"
         )
     if utils.verify_password_hash(user_credential.password, user.password):
         access_token = create_access_token(payload={"user_id": user.id})
