62a3a0ed13b6f309b078cdd3d2dbb227ee5af293
Kshitij Mathur <mathurk29@gmail.com>
2024-03-31 18:40:22 +0530

    alembic create users table

--- alembic/versions/0add1905a4b6_add_user_table.py
+++ alembic/versions/0add1905a4b6_add_user_table.py
@@ -5,22 +5,36 @@ Revises: 2f6b6b74c82b
 Create Date: 2024-03-31 18:33:00.595069
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '0add1905a4b6'
-down_revision: Union[str, None] = '2f6b6b74c82b'
+revision: str = "0add1905a4b6"
+down_revision: Union[str, None] = "2f6b6b74c82b"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    pass
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("email", sa.String(), nullable=False),
+        sa.Column("password", sa.String(), nullable=False),
+        sa.Column(
+            "created_at",
+            sa.TIMESTAMP(timezone=True),
+            server_default=sa.text("now()"),
+            nullable=False,
+        ),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("email"),
+    )
 
 
 def downgrade() -> None:
-    pass
+    op.drop_table("users")
