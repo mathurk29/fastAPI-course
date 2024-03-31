ebfa02c64a7fc892fef3aefe7c85539798311d8e
Kshitij Mathur <mathurk29@gmail.com>
2024-03-31 21:17:27 +0530

    somehow missed it - found as alembic autogenerated code for it

--- app/databases/models.py
+++ app/databases/models.py
@@ -44,7 +44,7 @@ class Posts(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, server_default="TRUE")
+    published = Column(Boolean, server_default="TRUE", nullable=False)
     created_at = Column(
         TIMESTAMP(timezone=True), server_default=text("NOW()"), nullable=False
     )
