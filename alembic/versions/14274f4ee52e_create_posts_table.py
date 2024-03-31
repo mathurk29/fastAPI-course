9f1d7f9e653d06586cc5d68fbbda225eeb4622fe
Kshitij Mathur <mathurk29@gmail.com>
2024-03-31 18:02:53 +0530

    first alembic revision

---
+++ alembic/versions/14274f4ee52e_create_posts_table.py
@@ -0,0 +1,27 @@
+"""Create posts table
+
+Revision ID: 14274f4ee52e
+Revises: 
+Create Date: 2024-03-31 15:32:37.478764
+
+"""
+
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "14274f4ee52e"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
