45f4e97b91ac145662066ddb65f3413be2fe2c0f
Kshitij Mathur <mathurk29@gmail.com>
2024-03-31 18:41:06 +0530

    alembic rev add FK to posts

---
+++ alembic/versions/b6ac8686ed7d_add_fk_to_posts_table.py
@@ -0,0 +1,26 @@
+"""add FK to posts table
+
+Revision ID: b6ac8686ed7d
+Revises: 0add1905a4b6
+Create Date: 2024-03-31 18:40:47.267370
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'b6ac8686ed7d'
+down_revision: Union[str, None] = '0add1905a4b6'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
