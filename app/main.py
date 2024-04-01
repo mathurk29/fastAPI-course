06ad85fa9eb64d19390419ff270a1b78d8364f04
Kshitij Mathur <mathurk29@gmail.com>
2024-04-01 14:58:46 +0530

    add CORS

--- app/main.py
+++ app/main.py
@@ -4,12 +4,28 @@ from controller.user import user_router
 from controller.vote import vote_router
 from databases import database_sqlalchemy, models
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # models.Base.metadata.create_all(bind=database_sqlalchemy.engine)
 
 app = FastAPI()
 
+origins = ["https://www.localhost.com/"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(crud_router)
 app.include_router(user_router)
 app.include_router(login_router)
 app.include_router(vote_router)
+
+
+@app.get("/")
+def root():
+    return "Hello World"
