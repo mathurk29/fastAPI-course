0ba46ef0f3cc65870947363a0c71869a67c38e10
Kshitij Mathur <mathurk29@gmail.com>
2024-05-15 15:04:06 +0530

    fixture for db client

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -8,15 +9,12 @@ from app.databases import database_sqlalchemy, models, schemas
 from app.databases.database_sqlalchemy import get_db
 from app.main import app
 
-client = TestClient(app)
 SQLALCHEMY_DATABASE_URL = f"{settings.database_type}://{settings.DB_USERNAME}:{settings.DB_PASSWORD}@{settings.DB_HOSTNAME}:{settings.DB_PORT}/{settings.DATABASE_NAME}_test"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(
     autoflush=False,
     bind=engine,
 )
-
-models.Base.metadata.create_all(bind=engine)
 
 
 # Dependency
@@ -31,12 +29,23 @@ def override_get_db():
 app.dependency_overrides[get_db] = override_get_db
 
 
-def test_root():
+@pytest.fixture
+def client():
+    models.Base.metadata.drop_all(bind=engine)
+    models.Base.metadata.create_all(bind=engine)
+    yield TestClient(app)
+    # or you can use alembic
+    # from alembic import command
+    # command.upgrade('head')
+    # command.downgrade('base')
+
+
+def test_root(client):
     res = client.get("/")
     assert res.json() == "Hello World"
 
 
-def test_create_user():
+def test_create_user(client):
     res = client.post("/users/", json={"email": "ksh2@gmail.com", "password": "123"})
     new_user = schemas.UserResponse(**res.json())
     assert new_user.email == "ksh2@gmail.com"
